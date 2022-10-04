1a34dc2fe274ff35c53064351a457c0611731d80
A-132001 <abdo01143617687@gmail.com>
2022-10-04 17:44:50 +0200

    Add Login with Email or User name

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/accounts/profile"
+AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
